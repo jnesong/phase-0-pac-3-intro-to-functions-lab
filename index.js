c27d83d13d277362c40b77747c5c82281c705207
Jenny Chau <jnechau@gmail.com>
2021-09-07 15:05:48 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+  } 
+
+  function whisper(string) {
+    return string.toLowerCase();
+  } 
+
+  console.log(shout('Hello!'));
+  console.log(whisper('Hello!'));
+
+  function logShout(string){
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(string){
+    console.log(string.toLowerCase());
+  }
+
+  logShout('Hello!');
+
+  function sayHiToGrandma(string){
+      if (string===string.toLowerCase()){
+          return "I can't hear you!"
+      }
+      if (string===string.toUpperCase()){
+          return "YES INDEED!"
+      }
+      if (string==="I love you, Grandma."){
+          return "I love you, too."
+      }
+  }
+
+  console.log(sayHiToGrandma("I love you, Grandma"));
